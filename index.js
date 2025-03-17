e0c9b8b405d547174df65b59376570d68de05f52
JohnAntz <johnantzoulisvazquez@outlook.com>
2025-03-17 14:29:04 +0100

    Fix the implementation of the gameBoard creation and styled a bit to make it visual

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 (() => {
-  const gameBoard = Array(9).fill({});
+  const gameBoard = document.getElementById("gameBoard");
+  const board = Array(9).fill({});
+  board.forEach(() => {
+    const cell = document.createElement("div");
+    cell.classList.add("board-cell");
+    gameBoard.appendChild(cell);
+  });
 })();
