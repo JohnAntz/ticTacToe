6604b1c51ad9e32e102ebb928ef9dc49b644a532
JohnAntz <johnantzoulisvazquez@outlook.com>
2025-03-19 16:33:17 +0100

    Change html structure to simplify js logic.

--- index.js
+++
@@ -1,9 +0,0 @@
-(() => {
-  const gameBoard = document.getElementById("gameBoard");
-  const board = Array(9).fill({});
-  board.forEach(() => {
-    const cell = document.createElement("div");
-    cell.classList.add("board-cell");
-    gameBoard.appendChild(cell);
-  });
-})();
